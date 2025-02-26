01fe2c4b3b21a987fa3ece94eca7bfdc59d6f13e
Timmarinho <timammf@gmail.com>
2025-02-25 22:17:13 -0300

    Add files via upload

---
+++ atividade aula 2 js.js
@@ -0,0 +1,14 @@
+function startNow() {
+    alert("O site ainda está em construção!");
+}
+function Home() {
+    alert("O site ainda está em construção");
+}
+
+function Sobre() {
+    alert("O site ainda está em construção");
+}
+
+function Treinar() {
+    alert("O site ainda está em construção");
+}
